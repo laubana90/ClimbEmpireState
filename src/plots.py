383208aee6a9b5966c2131479c3a690af75c2f4a
Laura Barranco Navarro <laubana90@gmail.com>
2022-02-09 23:31:07 +0100

    Upload project

---
+++ src/plots.py
@@ -0,0 +1,83 @@
+import matplotlib.pyplot as plt
+import os
+
+def plot_random_walk(random_walk, minimum):
+    '''
+    Create a simple line plot from the list that contains the steps
+    
+    Parameters
+    ----------
+    random_walk: list
+      Each element corresponds to each step from each dice roll.
+    minimum: int
+      Minimum step to reach at the end
+
+    '''
+    
+    plt.plot(random_walk)
+    
+    # Make the plot readable
+    plt.xlabel("Number of dice rolls")
+    plt.ylabel("Position")
+    plt.title("Random walk")
+    plt.axhline(minimum, color='Gray', linestyle='--', linewidth=0.7)
+    plt.text(0,minimum*0.93,"Want to have the end point above this mark")
+
+    # Save the figure
+    create_dir('figures')
+    plt.savefig('figures/random_walk.png')
+    
+def plot_multiple_random_walks(all_walks_np_t, minimum):
+    '''
+    Create multiple lines plot from the array that contains the several random walks
+    
+    Parameters
+    ----------
+    all_walks_np_t: 2d-numpy array
+      Each element corresponds to one complete random walk
+    minimum: int
+      Minimum step to reach at the end
+
+    '''
+    
+    plt.plot(all_walks_np_t)
+    
+    # Make the plot readable
+    plt.xlabel("Number of dice rolls")
+    plt.ylabel("Position")
+    plt.title("Random walk")
+    plt.axhline(minimum, color='Gray', linestyle='--', linewidth=0.7)
+    
+    # Save the figure
+    create_dir('figures')
+    plt.savefig('figures/multiple_random_walks.png')
+    
+def histogram_endpoints(ends, probability, minimum):
+    '''
+    Create a histogram from the end points of the set of random walks
+    
+    Parameters
+    ----------
+    ends: numpy array
+       Each element corresponds to the end-point of a random walk
+    minimum: int
+      Minimum step to reach at the end
+
+    '''
+    __, ax = plt.subplots()
+    plt.hist(ends, bins=20)
+    
+    # Make the plot readable
+    plt.xlabel("Final step")
+    plt.ylabel("Counts")
+    plt.title("Distribution of end points")
+    plt.text(0.05,0.8,"Probability: "+ str(probability)+"%", transform=ax.transAxes)
+    plt.axvline(minimum, color='Tomato', linestyle='--')
+ 
+    # Save the figure
+    create_dir('figures')
+    plt.savefig('figures/histogram_endpoints.png')
+
+def create_dir(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
---
+++ src/utils.py
@@ -0,0 +1,104 @@
+import numpy as np
+
+def simulate_random_walk(n_rolls):
+    '''
+    Simulates a random walk given a number of dice rolls and returns the outcome.
+    Takes into account that the lower possible step is 0 and that
+    there is a 0.1% chance of falling down and having to start from 0.
+    
+    Each movement is based in the outcome of a dice roll:
+    - 1/2 : go one step down
+    - 3/4/5: go one step up
+    - 6: throw again and go up the resulting number of steps
+    
+
+    Parameters
+    ----------
+    n_rolls: int
+      Number of dice rolls
+
+    Returns
+    -------
+    random_walk: list
+      Final walk. Each element corresponds to the actual step based on the
+      roll outcome and the previous position
+
+    '''
+    
+    random_walk = [0]
+    for n in range(n_rolls):
+        # Take as initial position the previous step
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
+        if dice <= 2:
+            # Prevent step from being negative
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1,7)
+        # Account for 0.1% chances of falling down
+        if np.random.rand() <= 0.001 :
+            step = 0
+        random_walk.append(step)
+    return random_walk
+    
+def generate_multiple_walks(n_pseudoexperiments, n_rolls):
+    '''
+    Generates a number of random walks and stores the outcomes
+    as a numpy array. It uses the function simulate_random_walk
+    
+    Parameters
+    ----------
+    n_pseudoexperiments: int
+      Number of pseudoexperiments
+    n_rolls: int
+      Number of dice rolls
+
+    Returns
+    -------
+    all_random_walk: 2d-numpy array
+      Each element corresponds to one complete random walk
+    ends: numpy array
+       Each element corresponds to the end-point of a random walk
+
+    '''
+    
+    # Generate the set of random walks
+    all_walks = []
+    for i in range(n_pseudoexperiments):
+        random_walk = simulate_random_walk(n_rolls)
+        # Create a list of lists
+        all_walks.append(random_walk)
+
+    # Transform all walks into a 2d-numpy array and transpose it
+    all_walks_np = np.array(all_walks)
+    all_walks_np_t = np.transpose(all_walks_np)
+
+    # Select last row from all_walks_np_t
+    ends = all_walks_np_t[-1,:]
+    
+    return all_walks_np_t, ends
+    
+def compute_probability(ends,n_pseudoexperiments, minimum):
+    '''
+    Compute the probability of reaching at least step 'minimum' after 'n_rolls' dice rolls
+    
+    Parameters
+    ----------
+    ends: numpy array
+       Each element corresponds to the end-point of a random walk
+    n_pseudoexperiments: int
+      Number of pseudoexperiments
+    minimum: int
+      Minimum step to reach at the end
+      
+    Returns
+    -------
+    probability: float
+        probability of reaching at least step 'minimum' after 'n_rolls' dice rolls
+
+    '''
+    probability = (np.count_nonzero(ends >= minimum)/n_pseudoexperiments)*100
+    
+    return probability
